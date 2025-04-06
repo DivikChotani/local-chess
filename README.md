407b54e8be1db158b5d68122e462ffd67adc6d15
Divik Chotani <divik.chotani@gmail.com>
2025-04-05 23:20:18 -0700

    added readme and beginning of chess script

---
+++ README.md
@@ -0,0 +1,4 @@
+# Local Chess
+
+A Python-based chess application that allows you to play chess against the Stockfish engine locally.
+
--- local_chess.py
+++ local_chess.py
@@ -2,3 +2,8 @@ import chess
 import chess.engine
 
 engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
+board = chess.Board()
+
+while not board.is_game_over():
+    result = engine.play(board, chess.engine.Limit(time=0.1))
+    board.push(result.move)
