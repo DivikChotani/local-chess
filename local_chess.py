8ab07999f95ca8ceddcc43283f89a2fac2f7dc92
Divik Chotani <divik.chotani@gmail.com>
2025-04-07 18:28:21 -0700

    added analysis and who's turn it is

--- local_chess.py
+++ local_chess.py
@@ -4,9 +4,19 @@ import chess.engine
 engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
 board = chess.Board()
 
+print(board)
+white = True
 while not board.is_game_over():
     result = engine.play(board, chess.engine.Limit(time=0.1))
+    info = engine.analyse(board, chess.engine.Limit(time=0.1))
+    print("White") if white==True else print("Black")
+    white = not white
+    print("best move:", result.move)
+    print("Score:", info["score"].white())
+
     board.push(result.move)
-    print(board)  # <- add this to see each move in text
+    print(board)
+
+
 
 engine.quit()
