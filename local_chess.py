4484e2c7c5ceb7a0adfbe9e3d40b69d4244f2a4b
Divik Chotani <divik.chotani@gmail.com>
2025-04-07 18:51:13 -0700

    added multiple move analysis - next step is to add move difficulty to analyze how good of a move the user played

--- local_chess.py
+++ local_chess.py
@@ -1,6 +1,5 @@
 import chess
 import chess.engine
-
 engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
 board = chess.Board()
 
@@ -8,11 +7,13 @@ print(board)
 white = True
 while not board.is_game_over():
     result = engine.play(board, chess.engine.Limit(time=0.1))
-    info = engine.analyse(board, chess.engine.Limit(time=0.1))
+    info = engine.analyse(board, chess.engine.Limit(time=0.1), multipv=10)
     print("White") if white==True else print("Black")
     white = not white
-    print("best move:", result.move)
-    print("Score:", info["score"].white())
+    for i, info in enumerate(info):
+        move = info["pv"][0] if "pv" in info else None
+        score = info["score"].white()
+        print(f"#{i+1} Best move: {move}, Score: {score}")
 
     board.push(result.move)
     print(board)
