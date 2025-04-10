fecd1648880762bb5693e11c4a8dafcaa3c482ba
Divik Chotani <divik.chotani@gmail.com>
2025-04-09 21:14:55 -0700

    made the code purely server based and added the sqlite library

--- local_chess.py
+++ local_chess.py
@@ -2,26 +2,31 @@ from flask import Flask, request, jsonify
 import chess
 import chess.engine
 import chess.svg
+import sqlite3
 
 engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
 
+conn = sqlite3.connect("chess-games.db")
+
+cursor = conn.cursor()
+
 # Start two Stockfish instances
-engine_white = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
-engine_black = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
-engine_white.configure({
-    "UCI_LimitStrength": True,
-    "UCI_Elo": 2500  # You can set 1350, 1600, etc. too
-})
-# Set weaker ELO for Black
-engine_black.configure({
-    "UCI_LimitStrength": True,
-    "UCI_Elo": 1320  # You can set 1350, 1600, etc. too
-})
+# engine_white = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
+# engine_black = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/bin/stockfish")
+# engine_white.configure({
+#     "UCI_LimitStrength": True,
+#     "UCI_Elo": 2500  # You can set 1350, 1600, etc. too
+# })
+# # Set weaker ELO for Black
+# engine_black.configure({
+#     "UCI_LimitStrength": True,
+#     "UCI_Elo": 1320  # You can set 1350, 1600, etc. too
+# })
 
 board = chess.Board()
-print("Starting position:")
-print(board)
-print(board.fen())
+# print("Starting position:")
+# print(board)
+# print(board.fen())
 
 app = Flask(__name__)
 @app.post('/initialize-board')
@@ -96,27 +101,30 @@ def getBestMoves():
         return jsonify({"error": str(e)}), 400
             
             
-while not board.is_game_over():
-    info = engine.analyse(board, chess.engine.Limit(time=0.1), multipv=10)
-    for i, info in enumerate(info):
-            move = info["pv"][0] if "pv" in info else None
-            score = info["score"].white()
-            print(f"#{i+1} Best move: {move}, Score: {score}")
-    if board.turn == chess.WHITE:
-        result = engine_white.play(board, chess.engine.Limit(time=0.1))
-        print(result)
+# while not board.is_game_over():
+#     info = engine.analyse(board, chess.engine.Limit(time=0.1), multipv=10)
+#     for i, info in enumerate(info):
+#             move = info["pv"][0] if "pv" in info else None
+#             score = info["score"].white()
+#             print(f"#{i+1} Best move: {move}, Score: {score}")
+#     if board.turn == chess.WHITE:
+#         result = engine_white.play(board, chess.engine.Limit(time=0.1))
+#         print(result)
 
-        print("White (Strong) plays:", result.move)
-    else:
-        result = engine_black.play(board, chess.engine.Limit(time=0.1))
-        print("Black (Weak) plays:", result.move)
-    board.push(result.move)
-    print(board)
+#         print("White (Strong) plays:", result.move)
+#     else:
+#         result = engine_black.play(board, chess.engine.Limit(time=0.1))
+#         print("Black (Weak) plays:", result.move)
+#     board.push(result.move)
+#     print(board)
 
-# Show result
-print("Game over:", board.result())
+# # Show result
+# print("Game over:", board.result())
 
 # Clean up
-engine_white.quit()
-engine_black.quit()
+conn.commit()
+conn.close()
+
+# engine_white.quit()
+# engine_black.quit()
 engine.quit()
